8f3dfd1ea479445382888d82ccc05f24a4926aa6
JiphTzu <jiphutzu@JiMBP.local>
2018-09-17 11:29:34 +0800

    添加异形图片区域识别；添加LowPolygon模型转换

---
+++ Assets/Editor/Tzu/LowPolyConverter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Tzu
+{
+    public class LowPolyConverter
+    {
+        [MenuItem("Tools/Tzu/ToLowPoly")]
+        private static void LowPoly()
+        {
+            Transform[] transforms = Selection.transforms;
+
+            for (int i = 0; i < transforms.Length; i++)
+            {
+                LowPoly(transforms[i].GetComponent<MeshFilter>());
+            }
+        }
+
+        private static void LowPoly(MeshFilter meshFilter)
+        {
+            if (meshFilter == null) return;
+            Mesh mesh = meshFilter.sharedMesh;
+
+            Vector3[] oldVerts = mesh.vertices;//保存当前Mesh顶点  
+            int[] triangles = mesh.triangles;//三角索引数组  
+            Vector2[] olduvs = mesh.uv;
+            BoneWeight[] oldBoneWeights = mesh.boneWeights;
+
+            int triangleCount = triangles.Length;
+
+            Vector3[] verts = new Vector3[triangleCount];//用于保存新的顶点信息 
+            Vector2[] uvs = new Vector2[triangleCount];//用于保存新的uv信息 
+            BoneWeight[] boneWeights = new BoneWeight[triangleCount];//用于保存新的骨骼信息 
+
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                int index = triangles[i];
+                verts[i] = oldVerts[index];
+                uvs[i] = olduvs[index];
+                boneWeights[i] = oldBoneWeights[index];
+                triangles[i] = i;
+            }
+
+            mesh.vertices = verts;//更新Mesh顶点  
+            mesh.triangles = triangles;//更新索引 
+            mesh.uv = uvs;//更新uv信息
+            mesh.boneWeights = boneWeights;//更新骨骼信息
+
+            mesh.RecalculateBounds();//重新计算边界  
+            mesh.RecalculateNormals();//重新计算法线  
+        }
+    }
+}
---
+++ Assets/Scripts/Tzu/UIs/PolygonImageFilter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+///====================================
+///接受异形的Image。忽略透明度在ignoreAlpha及以下的像素
+///Image Settings中的Read/Write enabled 要设置为true。
+///Image属性SpriteMode为Single，Raycast Target 取消勾选
+///====================================
+namespace Tzu.UIs
+{
+    [RequireComponent(typeof(Image))]
+    public class PolygonImageFilter : MonoBehaviour, ICanvasRaycastFilter
+    {
+        [Range(0, 1)]
+        public float ignoreAlpha = 0;
+        private Image _image;
+        private RectTransform _rectTrans;
+
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+            _rectTrans = _image.rectTransform;
+        }
+        // Parameters:
+        //   sp:
+        //     Screen position.
+        //
+        //   eventCamera:
+        //     Raycast camera.
+        public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+        {
+            if (!enabled) return true;
+            Sprite sprite = _image.overrideSprite;
+            if (sprite == null) return true;
+
+            Vector2 local;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTrans, sp, eventCamera, out local);
+            Rect rect = _rectTrans.rect;
+            Vector2 pivot = _rectTrans.pivot;
+            // Convert to lower left corner as reference point.
+            local.x += pivot.x * rect.width;
+            local.y += pivot.y * rect.height;
+
+            float u = local.x / rect.width;
+            float v = local.y / rect.height;
+
+            return sprite.texture.GetPixelBilinear(u, v).a > ignoreAlpha;
+        }
+    }
+}
